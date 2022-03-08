e5be1b89d20133aa14520dee6bd4b70f86f51ef0
ssecv <tanghe996@126.com>
2022-03-08 20:30:13 +0800

    code wihtout datasets

--- data.py
+++ data.py
@@ -205,9 +205,6 @@ class test_dataset:
             transforms.ToTensor(),
             transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])])
         self.gt_transform = transforms.ToTensor()
-        # self.gt_transform = transforms.Compose([
-        #     transforms.Resize((self.trainsize, self.trainsize)),
-        #     transforms.ToTensor()])
         self.depths_transform = transforms.Compose(
             [transforms.Resize((self.testsize, self.testsize)), transforms.ToTensor()])
         self.size = len(self.images)
